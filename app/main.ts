534e876c65f9d28956a45652278bb990a83b6cd8
Mihael Stormrage <mihan.msn@hotmail.com>
2024-04-08 06:05:55 +0700

    pass 2nd stage

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,7 @@
 import * as net from "node:net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+  connection.end('+PONG\r\n');
 });
 
 server.listen(6379, "127.0.0.1");
-
