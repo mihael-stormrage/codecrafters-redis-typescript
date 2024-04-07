32d14865164bbb2ccdafacc6d2ed18f824d8e70c
Mihael Stormrage <mihan.msn@hotmail.com>
2024-04-08 06:22:42 +0700

    multiple pings in connection

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,9 @@
 import * as net from "node:net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  connection.end('+PONG\r\n');
+  connection.on('data', () => {
+    connection.write('+PONG\r\n');
+  });
 });
 
 server.listen(6379, "127.0.0.1");
