bffeeaa134de5334d2b77563d1db48a19352ba8d
Mihael Stormrage <mihan.msn@hotmail.com>
2024-04-11 03:35:49 +0700

    expiry key interface

--- app/kv.ts
+++ app/kv.ts
@@ -1,3 +1,40 @@
-const cache: Record<string, string> = {};
+const cache: Record<string, CachedKey> = {};
 
-export default cache;
+class CachedKey {
+  #value: string;
+  expiry?: number;
+
+  constructor(
+    public readonly key: string,
+    value: string,
+    expiry?: number,
+  ) {
+    this.#value = value;
+    if (expiry) this.expiry = Date.now() + expiry;
+  }
+
+  isExpired() {
+    return !!this.expiry && this.expiry < Date.now();
+  }
+
+  get value(): string {
+    if (this.isExpired()) {
+      this[Symbol.dispose]();
+      return '';
+    }
+    return this.#value;
+  }
+
+  set value(v: string) {
+    this.#value = v;
+  }
+
+  toString = () => this.value;
+
+  [Symbol.dispose]() {
+    delete cache[this.key];
+    console.log(`Key "${this.key}" disposed`);
+  }
+}
+
+export { cache as default, CachedKey };
