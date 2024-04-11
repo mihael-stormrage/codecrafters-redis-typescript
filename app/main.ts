288750758fa6f624d776af1b808f2ce30b5f5466
Mihael Stormrage <mihan.msn@hotmail.com>
2024-04-12 00:27:56 +0700

    port cli argument

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,17 @@
 import { createServer } from 'node:net';
 import parse from './parser.ts';
 
+const defaultPort = 6379;
+
+const parseCliArgs = () => {
+  const args = Deno.args;
+  const portIndex = args.indexOf('--port') + 1;
+  const port = Number(args[portIndex] ?? defaultPort);
+  return { port };
+};
+
+const { port } = parseCliArgs();
+
 const server = createServer((connection) => {
   connection.on('data', (data) => {
     const queue = parse(data.toString());
@@ -10,4 +21,4 @@ const server = createServer((connection) => {
   });
 });
 
-server.listen(6379, '127.0.0.1');
+server.listen(port, '127.0.0.1');
