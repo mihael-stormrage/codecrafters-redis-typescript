086b89dc1d3e22b2448226adf8fb4210709fad28
Mihael Stormrage <mihan.msn@hotmail.com>
2024-04-12 07:10:23 +0700

    new Parser with FSM

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,11 @@
 import { createServer } from 'node:net';
 import { parseArgs } from 'std/cli/parse_args.ts';
-import ReplicaInfo from './Replica.ts';
 import parse from './parser.ts';
 
 const { port } = parseArgs(Deno.args, {
   string: ['port'],
   default: { port: 6379 },
 });
-
-export const replica: ReplicaInfo = {
-  role: 'master',
-};
 
 const server = createServer((connection) => {
   connection.on('data', (data) => {
