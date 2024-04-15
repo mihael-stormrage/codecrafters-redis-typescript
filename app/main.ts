3e5c9fe1e58c1da5dd5dc632c71c4e1f1b08c8f5
Mihael Stormrage <mihan.msn@hotmail.com>
2024-04-15 13:39:31 +0700

    replicaof flag

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,19 @@
 import { createServer } from 'node:net';
 import { parseArgs } from 'std/cli/parse_args.ts';
 import parse from './parser.ts';
+import replica from './Replica.ts';
 
-const { port } = parseArgs(Deno.args, {
-  string: ['port'],
+const { port, replicaof: masterHost, _:  masterPort} = parseArgs(Deno.args, {
+  string: ['port', 'replicaof'],
   default: { port: 6379 },
 });
+
+const greeting = `Listening on 127.0.0.1:${port}`;
+const replicaOf = `-- replica of: ${masterHost}:${masterPort}`;
+if (masterHost) {
+  replica.role = 'slave';
+  console.log(greeting, replicaOf);
+}
 
 const server = createServer((connection) => {
   connection.on('data', (data) => {
