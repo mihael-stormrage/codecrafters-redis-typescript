3f19f2b7f785e98494d904952e6e3006b86f715d
Mihael Stormrage <mihan.msn@hotmail.com>
2024-04-16 12:09:05 +0700

    replica fields

--- app/Replica.ts
+++ app/Replica.ts
@@ -1,10 +1,14 @@
 interface ReplicaInfo {
   role: 'master' | 'slave';
+  master_replid: string,
+  master_repl_offset: number,
 }
 
-export const replica: ReplicaInfo = {
+const replica: ReplicaInfo = {
   role: 'master',
-};
+  master_replid: '8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb',
+  master_repl_offset: 0,
+}
 
 export default replica;
 export type { ReplicaInfo };
